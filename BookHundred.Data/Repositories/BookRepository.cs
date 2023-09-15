43e6494ecca3764b257fd581c721ead0fb1e747e
Ravindra Devrani <ravindra.devrani003@gmail.com>
2023-09-15 22:47:35 +0530

    branch for YT video

--- BookHundred.Data/Repositories/BookRepository.cs
+++ BookHundred.Data/Repositories/BookRepository.cs
@@ -25,49 +25,4 @@ public class BookRepository : IBookRepository
         return new BookResult { Books = books, PaginationData = paginationData };
     }
 
-    public async Task<Book> GetBook(int id)
-    {
-        using IDbConnection connection = new SqlConnection(_connectionString);
-        var books = await connection.QueryAsync<Book>("spGetBookById", new { id }, commandType: CommandType.StoredProcedure);
-        return books.FirstOrDefault();
-    }
-
-    public async Task DeleteBook(int id)
-    {
-        using IDbConnection connection = new SqlConnection(_connectionString);
-        var books = await connection.ExecuteAsync("spDeleteBook", new { id }, commandType: CommandType.StoredProcedure);
-    }
-
-    public async Task<Book> AddBook(Book book)
-    {
-        using IDbConnection connection = new SqlConnection(_connectionString);
-        var parameters = new DynamicParameters();
-        parameters.Add("Id", 0, DbType.Int32, ParameterDirection.Output);
-        parameters.Add("Title", book.Title);
-        parameters.Add("Author", book.Author);
-        parameters.Add("Country", book.Country);
-        parameters.Add("Language", book.Language);
-        parameters.Add("Link", book.Link);
-        parameters.Add("ImageLink", book.ImageLink);
-        parameters.Add("Price", book.Price);
-        parameters.Add("Pages", book.Pages);
-        parameters.Add("Year", book.Year);
-        await connection.ExecuteAsync("spAddBook", param: parameters, commandType: CommandType.StoredProcedure);
-        book.Id = parameters.Get<int>("Id");
-        return book;
-    }
-
-    public async Task UpdateBook(Book book)
-    {
-        using IDbConnection connection = new SqlConnection(_connectionString);
-        await connection.ExecuteAsync("spUpdateBook", book, commandType: CommandType.StoredProcedure);
-    }
-
-    public async Task<IEnumerable<string>> GetBookLanguages()
-    {
-        using IDbConnection connection = new SqlConnection(_connectionString);
-        var languages = await connection.QueryAsync<string>("spGetLanguages", commandType: CommandType.StoredProcedure);
-        return languages;
-    }
-
 }
--- BookHundred.Data/Repositories/IBookRepository.cs
+++ BookHundred.Data/Repositories/IBookRepository.cs
@@ -4,11 +4,6 @@ namespace BookHundred.Data.Repositories
 {
     public interface IBookRepository
     {
-        Task<Book> AddBook(Book book);
-        Task DeleteBook(int id);
-        Task<Book> GetBook(int id);
         Task<BookResult> GetBooksAsync(int page, int limit, string? searchTerm, string? languages, string? sortColumn, string? sortDirection);
-        Task UpdateBook(Book book);
-        Task<IEnumerable<string>> GetBookLanguages();
     }
 }
